97d1efcd0eca8c8a2a609d908e3838458b885cc7
jacek.slomian <Jacek.Slomian@exorigo-upos.pl>
2023-07-04 13:03:46 +0200

    checking if textInput works, testing

---
+++ PHP_tasks/task_2.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>task 2</title>
+</head>
+
+<body>
+    <?php
+    //1 - creating 2 classes as controls - TextInput & NumericInput
+    class TextInput
+    {
+        protected $value = '';
+
+        public function add($text)
+        {
+            $this->value .= $text;
+        }
+
+        public function getValue()
+        {
+            return $this->value;
+        }
+    }
+
+
+    //1,3 - NumericInput extends from TextInput
+    class NumericInput extends TextInput
+    {
+    }
+
+
+
+    $textInput = new TextInput();
+    $textInput->add('Hello');
+    $textInput->add(' World');
+    $textInput->add('123');
+    echo $textInput->getValue();
+
+    ?>
+</body>
+
+</html>
